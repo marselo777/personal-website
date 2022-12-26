d54cc2786f57f16861cdf0702aad7ced14aed9ef
Evgenii Frolov <evgenii.frolov@smile2impress.com>
2022-12-26 15:44:22 +0300

    styles fixes

--- src/components/sections/Contact/Contact.tsx
+++ src/components/sections/Contact/Contact.tsx
@@ -11,9 +11,9 @@ export const Contact = () => {
             <Typography className={styles.title}>What's Next?</Typography>
             <Typography className={styles.subtitle}>Get In Touch</Typography>
             <Typography className={styles.description}>
-                I'm looking for an intresting project with hard tasks. If you
-                have an highload project or you have an intresting project write
-                me an email!
+                I'm looking for an interesting project with hard tasks. If you
+                have an highload project or you have an interesting project
+                write me an email!
             </Typography>
             <Button component="a" href="mailto:yevgenii.frolov@gmail.com">
                 Say hello

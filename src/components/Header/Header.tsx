92d03ce8a2cbff4d81ea7a4ffec80ded1017637b
Evgenii Frolov <evgenii.frolov@smile2impress.com>
2022-12-26 15:32:34 +0300

    hide lng selector

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,24 +2,24 @@ import React from "react";
 
 import { HamburgerMenu } from "components/HamburgerMenu";
 import { Icon } from "components/Icon";
-import { LanguageSelect } from "components/LanguageSelect";
+// import { LanguageSelect } from "components/LanguageSelect";
 import { Nav } from "components/Nav";
 
 import styles from "./Header.module.scss";
 import { HeaderProps } from "./Header.types";
 
 export const Header = (props: HeaderProps) => {
-    const { routes, languagesList, defaultLanguage, onLanguageChange } = props;
+    const { routes } = props;
     return (
         <header className={styles.root}>
             <Icon iconName="Main" />
             <div className={styles.nav}>
                 <Nav links={routes} />
-                <LanguageSelect
+                {/* <LanguageSelect
                     defaultLanguage={defaultLanguage}
                     languages={languagesList}
                     onLanguageChange={onLanguageChange}
-                />
+                /> */}
             </div>
             <HamburgerMenu links={routes} />
         </header>
